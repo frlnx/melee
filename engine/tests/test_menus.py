1c6d4067b0e3a81cd17f44de42c55722a761efc8
Mikael Bååth <mikaelbaath@gmail.com>
2018-03-17 13:14:33 +0100

    Network connect works with new menus

---
+++ engine/tests/test_menus.py
@@ -0,0 +1,33 @@
+from engine.views import menus
+
+
+class TestBaseMenu(object):
+
+    def setup(self):
+
+        def test_func():
+            pass
+
+        self.target = menus.BaseMenu.labeled_menu_from_function_names("test", [test_func], 0, 0)
+
+    def test_menu_has_a_button(self):
+        assert len(self.target.buttons) == 1
+
+
+class TestInputMenu(object):
+
+    def setup(self):
+
+        def arg_func(name: str="foo", age: int=8000):
+            pass
+
+        def cancel_func():
+            pass
+
+        self.target = menus.InputMenu.input_menu("test", arg_func, 0, 0, cancel_func, 36)
+
+    def test_menu_has_two_input_fields(self):
+        assert len(self.target.inputboxes) == 2
+
+    def test_menu_func_is_callable(self):
+        self.target.buttons[0].func()
