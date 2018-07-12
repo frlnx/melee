e37caa664adb09378fe6bb3872145a46b52e9786
Mikael Bååth <mikaelbaath@gmail.com>
2018-07-12 13:31:00 +0200

    fixing tests

--- engine/tests/test_ship_part.py
+++ engine/tests/test_ship_part.py
@@ -12,45 +12,47 @@ class TestHorizontalEngineAt135DegreesOff(object):
 
     def setup(self):
         model = model_factory.manufacture("engine", position=[1, 0, -1], rotation=[0, 90, 0])
-        self.gamepad = InputHandler()
-        self.target = controller_factory.manufacture(model, self.gamepad)
+        self.target = model
+        self.target.set_state("active")
+        self.target.set_input_value(1.0)
 
-    def etst_engine_force_offsets_is_left(self):
-        x, y, z = self.target.moment_force.forces
-        assert (-1, 0, 0) == (x, y, z)
+    def test_engine_force_offsets_is_left(self):
+        x, y, z = self.target.acceleration
+        assert (-5.0, 0, 0) == tuple(round(v) for v in (x, y, z))
 
     def test_engine_force_direction_is_left(self):
-        assert round(self.target.moment_force.forces.direction.yaw, 3) == 90
+        assert round(self.target.acceleration.direction.yaw, 3) == 90
 
     def test_force_is_pushing_left(self):
-        assert -1 == self.target.moment_force.forces.x
+        assert -5.0 == self.target.acceleration.x
 
     def test_force_degree_is_positive_45_degrees_from_being_lateral(self):
-        assert degrees(self.target.moment_force.radians_force_is_lateral_to_position()) == 45
+        assert 135 == self.target.diff_yaw_of_force_to_pos  # Was 45 before refactor
 
     def test_force_is_rotating_left(self):
-        assert round(self.target.moment_force.delta_yaw, 2) == 26.57
+        assert 132.83 == round(self.target.torque.yaw, 2)
 
 
 class TestHorizontalEngineAt45DegreesOff(object):
 
     def setup(self):
         model = model_factory.manufacture("engine", position=[1, 0, -1], rotation=[0, 0, 0])
-        self.gamepad = InputHandler()
-        self.target = controller_factory.manufacture(model, self.gamepad)
+        self.target = model
+        self.target.set_state("active")
+        self.target.set_input_value(1.0)
 
     def test_engine_force_offsets_is_up(self):
-        x, y, z = self.target.moment_force.forces
-        assert (0, 0, -1) == (x, y, z)
+        x, y, z = self.target.acceleration
+        assert (0, 0, -5.0) == tuple(round(v) for v in (x, y, z))
 
     def test_engine_force_direction_is_left(self):
-        assert round(self.target.moment_force.forces.direction.yaw, 3) == 0
+        assert round(self.target.acceleration.direction.yaw, 3) == 0
 
     def test_force_is_pushing_up(self):
-        assert -1 == self.target.moment_force.forces.z
+        assert -5.0 == self.target.acceleration.z
 
     def test_force_degree_offset_is_negative_45_degrees_from_being_lateral(self):
-        assert degrees(self.target.moment_force.radians_force_is_lateral_to_position()) == -45
+        assert 45 == self.target.diff_yaw_of_force_to_pos
 
     def test_force_is_rotating_left(self):
-        assert round(self.target.moment_force.delta_yaw, 2) == 26.57
+        assert 132.83 == round(self.target.torque.yaw, 2)
